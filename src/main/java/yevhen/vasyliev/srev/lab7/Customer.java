302e4483e6b0603ad61d05c050daae088e4ce6dc
Yevhen Vasyliev <ye.vasyliev@gmail.com>
2020-11-29 12:32:04 +0200

    Duplicated Code

--- src/main/java/yevhen/vasyliev/srev/lab7/Customer.java
+++ src/main/java/yevhen/vasyliev/srev/lab7/Customer.java
@@ -30,46 +30,29 @@ public class Customer {
         if (!account.getCurrency().equals(currency)) {
             throw new RuntimeException("Can't extract withdraw " + currency);
         }
-        if (account.getType().isPremium()) {
-            switch (customerType) {
-                case COMPANY:
-                    // we are in overdraft
-                    if (account.getMoney() < 0) {
-                        // 50 percent discount for overdraft for premium account
-                        account.setMoney((account.getMoney() - sum) - sum * account.overdraftFee() * companyOverdraftDiscount / 2);
-                    } else {
-                        account.setMoney(account.getMoney() - sum);
-                    }
-                    break;
-                case PERSON:
-                    // we are in overdraft
-                    if (account.getMoney() < 0) {
-                        account.setMoney((account.getMoney() - sum) - sum * account.overdraftFee());
-                    } else {
-                        account.setMoney(account.getMoney() - sum);
-                    }
-                    break;
-            }
-        } else {
-            switch (customerType) {
-                case COMPANY:
-                    // we are in overdraft
-                    if (account.getMoney() < 0) {
-                        // no discount for overdraft for not premium account
-                        account.setMoney((account.getMoney() - sum) - sum * account.overdraftFee() * companyOverdraftDiscount);
-                    } else {
-                        account.setMoney(account.getMoney() - sum);
-                    }
-                    break;
-                case PERSON:
-                    // we are in overdraft
-                    if (account.getMoney() < 0) {
-                        account.setMoney((account.getMoney() - sum) - sum * account.overdraftFee());
-                    } else {
-                        account.setMoney(account.getMoney() - sum);
-                    }
-                    break;
-            }
+        withdraw(sum, companyOverdraftDiscount / (account.getType().isPremium() ? 2 : 1));
+    }
+
+    private void withdraw(double sum, double companyOverdraftDiscount) {
+        switch (customerType) {
+            case COMPANY:
+                // we are in overdraft
+                if (account.getMoney() < 0) {
+                    // 50 percent discount for overdraft for premium account
+                    // no discount for overdraft for not premium account
+                    account.setMoney((account.getMoney() - sum) - sum * account.overdraftFee() * companyOverdraftDiscount);
+                } else {
+                    account.setMoney(account.getMoney() - sum);
+                }
+                break;
+            case PERSON:
+                // we are in overdraft
+                if (account.getMoney() < 0) {
+                    account.setMoney((account.getMoney() - sum) - sum * account.overdraftFee());
+                } else {
+                    account.setMoney(account.getMoney() - sum);
+                }
+                break;
         }
     }
 
@@ -98,14 +81,18 @@ public class Customer {
     }
 
     public String printCustomerDaysOverdrawn() {
-        String fullName = name + " " + surname + " ";
+        String fullName = getFullName();
 
         String accountDescription = "Account: IBAN: " + account.getIban() + ", Days Overdrawn: " + account.getDaysOverdrawn();
         return fullName + accountDescription;
     }
 
+    private String getFullName() {
+        return name + " " + surname + " ";
+    }
+
     public String printCustomerMoney() {
-        String fullName = name + " " + surname + " ";
+        String fullName = getFullName();
         String accountDescription = "";
         accountDescription += "Account: IBAN: " + account.getIban() + ", Money: " + account.getMoney();
         return fullName + accountDescription;
